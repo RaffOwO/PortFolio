efbe9acac9c640970e0bf4b9c5027110a0a7ba44
Raffaele Castellano <raffaelecastellano213@gmail.com>
2022-07-05 12:11:11 +0200

    Initial commit, index and his style

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<head>
+    
+</head>
+
+<body>
+    <header>
+        <?php
+            include("views/header.html")
+        ?>
+    </header>
+
+    <main>
+
+    </main>
+
+    <footer>
+        <?php
+            include("views/footer.html")
+        ?>
+    </footer>
+</body>
